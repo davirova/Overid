c8c7af1e9e33ee7f8bdf9da22620f95f1135465c
Davirova Kamola <davirovakamola9702@gmail.com>
2024-04-22 23:25:51 +0500

    yes

--- src/GeometricFigure.java
+++ src/GeometricFigure.java
@@ -3,10 +3,10 @@ public  class GeometricFigure {
      public static double sideB;
      public static double sideC;
      static double high;
-     void area(double sideA, double sideB,double sideC){
+     public void area(double sideA, double sideB,double sideC){
          System.out.println("Yuas: ");
      }
-     void perimetr(double sideA, double sideB,double high){
+     public void perimetr(double sideA, double sideB,double high){
          System.out.println("Perimetri: ");
      }
 
